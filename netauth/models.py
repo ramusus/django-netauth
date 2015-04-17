1d0d54783aa797512073b52d7733d8936982710d
ramusus <ramusus@gmail.com>
2015-04-17 16:15:28 +0700

    update for custom auth user model

--- netauth/models.py
+++ netauth/models.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 
 
 class NetID(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     identity = models.CharField(u'Net ID', max_length=255, unique=True)
     provider = models.CharField(u'Net authentication provider name', max_length=255)
 
